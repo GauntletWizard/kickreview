ce0917ac2e3829890a60f1fc378019f3ef02485b
Ted Hahn <thahn@tcbtech.com>
2012-12-06 16:01:47 -0800

    Data model for Project.

--- userscript/kickback.js
+++ userscript/kickback.js
@@ -8,10 +8,22 @@ backinglist = document.getElementsByClassName("backings")[0].children[1]
 
 function grabPage(pagenum) {
   var request = new XMLHttpRequest;
-  request.open("GET", root + String(pagenum));
+  request.open("GET", root + String(pagenum), false);
   request.send();
   var doc = document.implementation.createHTMLDocument("example");
   doc.documentElement.innerHTML = request.response;
 
   return doc
+}
+
+//This is super fragile, as parsing goes, but they don't have CSS classes.
+function project(row) {
+  this.projectName = row.children[0].innerText;
+  this.projectLink = row.children[0].href;
+  this.status = row.children[1].children[0].innerText;
+  this.endDate = row.children[2].innerText;
+  this.pledge = row.children[3].children[0].innerText;
+  this.pledgeDate = row.children[3].children[2].innerText;
+  this.pledgeStatus = row.children[4].innerText;
+  this.reward = row.children[5].innerText;
 }
