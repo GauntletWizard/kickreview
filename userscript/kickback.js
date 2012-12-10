bd40b6fb4aa186dac3317a9107027f1706f1012a
Thomas Hahn <thahn@tcbtech.com>
2012-12-10 03:22:33 -0800

    Hangs the browser
    Doesn't work as an extension, and doesn't do anything but lock yet, but
    eh, that's what debugging's for :)

--- userscript/kickback.js
+++ userscript/kickback.js
@@ -1,10 +1,9 @@
 /**
- * @fileoverview Description of this file.
+ * @fileoverview 
  */
 
 root = "https://www.kickstarter.com/profile/transactions?page="
-htmldtd = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">'
-backinglist = document.getElementsByClassName("backings")[0].children[1]
+// htmldtd = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">'
 
 function grabPage(pagenum) {
   var request = new XMLHttpRequest;
@@ -22,8 +21,28 @@ function project(row) {
   this.projectLink = row.children[0].href;
   this.status = row.children[1].children[0].innerText;
   this.endDate = row.children[2].innerText;
-  this.pledge = row.children[3].children[0].innerText;
+  this.pledge = Number(row.children[3].children[0].innerText.substr(1));
   this.pledgeDate = row.children[3].children[2].innerText;
   this.pledgeStatus = row.children[4].innerText;
   this.reward = row.children[5].innerText;
+}
+
+function isLastPage(page) {
+  return page.getElementsByClassName("no-content").length == 1;
+}
+
+function totals() {
+  var p = 1;
+  total = 0;
+  
+  var page = grabPage(p)
+  while (!isLastPage(page)) {
+    
+    backinglist = page.getElementsByClassName("backings")[0].children[1]
+    for (i = 0; i < backinglist.length; i++) {
+      pj = new project(backinglist[i]);
+      total = total + pj.pledge;
+    }
+  }
+  return total;
 }
